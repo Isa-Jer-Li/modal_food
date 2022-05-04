e480f2b50e4f7915222d54ca0e616acbfa7339fd
Isabelle <hatingusernames97@gmail.com>
2022-05-04 11:56:04 -0500

    Start up modal

---
+++ script.js
@@ -0,0 +1,31 @@
+const modal = document.getElementById("modal")
+function openModal() {
+	modal.style.display = "block"
+}
+
+function closeModal() {
+	modal.style.display = "none"
+}
+
+// bd
+async function ButtonClicked() {
+
+	let values = []
+	let checkboxes = document.querySelectorAll('input:checked')
+
+  checkboxes.forEach((checkbox) => {
+		values.push(checkbox.value)
+	})
+
+   values.join(',')
+
+
+  console.log('values', values)
+
+  const title = document.getElementById('title')
+  const ingredients =
+   document.getElementById('ingredients')
+  const nutrition = document.getElementById('nutrition')
+  const prep = document.getElementById('prep')
+  const time = document.getElementById('time')
+  const error = document.getElementById('error')
