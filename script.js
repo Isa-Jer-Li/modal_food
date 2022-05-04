14d001e8f82487154718553ad8d571f219f13da6
Isabelle <hatingusernames97@gmail.com>
2022-05-04 12:03:01 -0500

    Made modal easier to read

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ function closeModal() {
 }
 
 // bd
-async function ButtonClicked() {
-
-	let values = []
-	let checkboxes = document.querySelectorAll('input:checked')
-
-  checkboxes.forEach((checkbox) => {
-		values.push(checkbox.value)
-	})
-
-   values.join(',')
-
-
-  console.log('values', values)
-
-  const title = document.getElementById('title')
-  const ingredients =
-   document.getElementById('ingredients')
-  const nutrition = document.getElementById('nutrition')
-  const prep = document.getElementById('prep')
-  const time = document.getElementById('time')
-  const error = document.getElementById('error')
+// async function ButtonClicked() {
+//
+// 	let values = []
+// 	let checkboxes = document.querySelectorAll('input:checked')
+//
+//   checkboxes.forEach((checkbox) => {
+// 		values.push(checkbox.value)
+// 	})
+//
+//    values.join(',')
+//
+//
+//   console.log('values', values)
+//
+//   const title = document.getElementById('title')
+//   const ingredients =
+//    document.getElementById('ingredients')
+//   const nutrition = document.getElementById('nutrition')
+//   const prep = document.getElementById('prep')
+//   const time = document.getElementById('time')
+//   const error = document.getElementById('error')
